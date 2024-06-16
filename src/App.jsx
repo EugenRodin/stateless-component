a1a34489d966d47d50007fa01a904fc8a303beb4
EugenRodin <gameover59630@gmail.com>
2024-06-16 23:37:59 +0200

    ыефеÑstateless-component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextDisplay from './components/TextDisplay'
+
+function App() {
+    return (
+        <div>
+            <TextDisplay message="Hello, this is a stateless component!" />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/TextDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TextDisplay({ message }) {
+    return (
+        <div>
+            <h1>Text</h1>
+            <p>{message}</p>
+        </div>
+    )
+}
+
+export default TextDisplay
